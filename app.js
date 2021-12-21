9edc6f48c88cda9931544d527b40f92ba27c42e8
Eduardo Gordillo <edu.m.gordillo@gmail.com>
2021-12-21 09:55:32 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+
+     
+        let score = 0 ;
+        
+        AFRAME.registerComponent("shootable", {
+            init: function(){
+
+                this.el.onclick = () => {
+                    score++
+                    document.querySelector('[text]').setAttribute('value', score + " barcos derribados")
+                    this.el.removeAttribute('shootable')
+                    this.el.setAttribute('animation', 'property: position; to: 0 -300 0; dur: 4000')
+                                        
+                }
+            }
+        })
+  
